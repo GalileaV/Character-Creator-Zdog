4048002dcf5ba4646d1b5bf5d61d552925fe5a1b
Galiov <galilea.valle@gmail.com>
2019-06-18 13:34:57 -0500

    Moved gender selection functions to its own file

---
+++ gender-selector.js
@@ -0,0 +1,16 @@
+import { maleBody } from './Shapes/Gender/male-body.js';
+import { femaleBody } from './Shapes/Gender/female-body.js';
+import { defaultEyes, happyEyes, lineEyes } from './Shapes/Face/eyes.js';
+
+export const selectMale = (illo) => {
+  femaleBody.remove();
+  illo.addChild(maleBody);
+  illo.addChild(defaultEyes);
+};
+
+export const selectFemale = (illo) => {
+  maleBody.remove();
+  illo.addChild(femaleBody);
+  illo.addChild(defaultEyes);
+
+};
--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-import { maleBody } from './Shapes/Gender/male-body.js';
-import { femaleBody } from './Shapes/Gender/female-body.js';
-import { defaultEyes, happyEyes, lineEyes } from './Shapes/Face/eyes.js';
+import { selectMale, selectFemale } from './gender-selector.js';
  
-const illo = new Zdog.Illustration({
+const illustration = new Zdog.Illustration({
   element: '.zdog-canvas',
   zoom: 9,
   dragRotate: true,
 });
 
-const selectMale = () => {
-  femaleBody.remove();
-  illo.addChild(maleBody);
-  illo.addChild(defaultEyes);
-};
-document.querySelector('.male-body').addEventListener('click', selectMale);
+document.querySelector('.male-body').addEventListener('click', () => selectMale(illustration));
 
-const selectFemale = () => {
-  maleBody.remove();
-  illo.addChild(femaleBody);
-  illo.addChild(defaultEyes);
 
-};
-document.querySelector('.female-body').addEventListener('click', selectFemale);
+document.querySelector('.female-body').addEventListener('click', () => selectFemale(illustration));
 
 
 
 const animate = () => {
-  illo.updateRenderGraph();
+  illustration.updateRenderGraph();
   requestAnimationFrame( animate );
 };
 
