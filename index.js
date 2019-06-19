74dcef76ac240f2953d8c63540dfe7cb44c26ebc
Galiov <galilea.valle@gmail.com>
2019-06-19 13:25:23 -0500

    Added mouth-selector function and its buttons to select it

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { selectMale, selectFemale } from './Selectors/gender-selector.js';
 import { eyeSelect } from './Selectors/eye-selector.js';
+import { mouthSelect } from './Selectors/mouth-selector.js';
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
@@ -19,6 +20,14 @@ $('.strong-eyes').addEventListener('click', () => eyeSelect("strong", illustrati
 $('.tired-eyes').addEventListener('click', () => eyeSelect("tired", illustration));
 $('.angry-eyes').addEventListener('click', () => eyeSelect("angry", illustration));
 $('.lashes-eyes').addEventListener('click', () => eyeSelect("lashes", illustration));
+
+$('.default-mouth').addEventListener('click', () => mouthSelect("default", illustration));
+$('.happy-mouth').addEventListener('click', () => mouthSelect("happy", illustration));
+$('.line-mouth').addEventListener('click', () => mouthSelect("line", illustration));
+$('.doth-mouth').addEventListener('click', () => mouthSelect("doth", illustration));
+$('.sad-mouth').addEventListener('click', () => mouthSelect("sad", illustration));
+$('.sorprise-mouth').addEventListener('click', () => mouthSelect("sorprise", illustration));
+$('.disgust-mouth').addEventListener('click', () => mouthSelect("disgust", illustration));
 
 $('.male-body').addEventListener('click', () => selectMale(illustration));
 $('.female-body').addEventListener('click', () => selectFemale(illustration));
